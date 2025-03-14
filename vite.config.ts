9ad7fb40872a765ecae8d6c50339f9bc50db56e1
SnB <skullandbonesdao@gmail.com>
2025-03-14 02:42:56 +0100

    Set base URL and standardize quotes in Vite config
    Updated the base URL to "/track.groceries.app/" for correct asset resolution. Standardized all quotes to double quotes for consistency throughout the configuration file.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import path from 'node:path'
-import vue from '@vitejs/plugin-vue'
-import autoprefixer from 'autoprefixer'
-import tailwind from 'tailwindcss'
-import { defineConfig } from 'vite'
+import path from "node:path";
+import vue from "@vitejs/plugin-vue";
+import autoprefixer from "autoprefixer";
+import tailwind from "tailwindcss";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-
+  base: "/track.groceries.app/",
   css: {
     postcss: {
       plugins: [tailwind(), autoprefixer()],
@@ -15,7 +15,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
