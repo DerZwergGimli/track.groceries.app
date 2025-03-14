d2dcbfa9511a397f438f50b758aea20ec75d1b63
SnB <skullandbonesdao@gmail.com>
2025-03-14 03:27:43 +0100

    Wrap tables in card components for consistent UI styling.
    Replaced `TableCaption` with `Card` components to provide a more cohesive and unified look for the group and tag lists. This change improves the visual structure and aligns with the application's design system.

--- src/components/elements/list/ListGroups.vue
+++ src/components/elements/list/ListGroups.vue
@@ -2,7 +2,6 @@
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
@@ -13,47 +12,49 @@ import { Button } from "@/components/ui/button";
 import { useSupabaseStore } from "@/stores/SupabaseStore.ts";
 import { Icon } from "@iconify/vue";
 import { Input } from "@/components/ui/input";
+import { Card } from "@/components/ui/card";
 </script>
 
 <template>
-  <Table>
-    <TableCaption>A list of your groups.</TableCaption>
-    <TableHeader>
-      <TableRow>
-        <TableHead class="w-[100px]">Name</TableHead>
-        <TableHead>Description</TableHead>
-        <TableHead class="text-right"> Action</TableHead>
-      </TableRow>
-    </TableHeader>
+  <Card>
+    <Table class="">
+      <TableHeader>
+        <TableRow>
+          <TableHead class="w-[100px]">Name</TableHead>
+          <TableHead>Description</TableHead>
+          <TableHead class="text-right"></TableHead>
+        </TableRow>
+      </TableHeader>
 
-    <TableBody>
-      <TableRow v-for="group in useSupabaseStore().groups" :key="group.id">
-        <TableCell class="font-medium">
-          <Input
-            @change="useSupabaseStore().updateGroup(group, group.id)"
-            type="text"
-            v-model="group.name"
-          />
-        </TableCell>
-        <TableCell
-          ><Input
-            @change="useSupabaseStore().updateGroup(group, group.id)"
-            type="text"
-            v-model="group.description"
-        /></TableCell>
-        <TableCell class="text-right">
-          <Button
-            variant="outline"
-            @click.prevent="useSupabaseStore().deleteGroup(group.id)"
-          >
-            <Icon
-              icon="solar:trash-bin-minimalistic-outline"
-              width="24"
-              height="24"
+      <TableBody>
+        <TableRow v-for="group in useSupabaseStore().groups" :key="group.id">
+          <TableCell class="font-medium">
+            <Input
+              @change="useSupabaseStore().updateGroup(group, group.id)"
+              type="text"
+              v-model="group.name"
             />
-          </Button>
-        </TableCell>
-      </TableRow>
-    </TableBody>
-  </Table>
+          </TableCell>
+          <TableCell
+            ><Input
+              @change="useSupabaseStore().updateGroup(group, group.id)"
+              type="text"
+              v-model="group.description"
+          /></TableCell>
+          <TableCell class="text-right">
+            <Button
+              variant="outline"
+              @click.prevent="useSupabaseStore().deleteGroup(group.id)"
+            >
+              <Icon
+                icon="solar:trash-bin-minimalistic-outline"
+                width="24"
+                height="24"
+              />
+            </Button>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </Card>
 </template>
--- src/components/elements/list/ListTags.vue
+++ src/components/elements/list/ListTags.vue
@@ -2,7 +2,6 @@
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
@@ -13,48 +12,50 @@ import { Button } from "@/components/ui/button";
 import { useSupabaseStore } from "@/stores/SupabaseStore.ts";
 import { Icon } from "@iconify/vue";
 import { Input } from "@/components/ui/input";
+import { Card } from "@/components/ui/card";
 </script>
 
 <template>
-  <Table>
-    <TableCaption>A list of your tags.</TableCaption>
-    <TableHeader>
-      <TableRow>
-        <TableHead class="w-[100px]">Name</TableHead>
-        <TableHead>Description</TableHead>
-        <TableHead class="text-right"> Action</TableHead>
-      </TableRow>
-    </TableHeader>
+  <Card>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead class="w-[100px]">Name</TableHead>
+          <TableHead>Description</TableHead>
+          <TableHead class="text-right"></TableHead>
+        </TableRow>
+      </TableHeader>
 
-    <TableBody>
-      <TableRow v-for="tag in useSupabaseStore().tags" :key="tag.id">
-        <TableCell class="font-medium">
-          <Input
-            type="text"
-            @change="useSupabaseStore().updateTag(tag, tag.id)"
-            v-model="tag.name"
-          />
-        </TableCell>
-        <TableCell
-          ><Input
-            @change="useSupabaseStore().updateTag(tag, tag.id)"
-            type="text"
-            v-model="tag.description"
-          />
-        </TableCell>
-        <TableCell class="text-right">
-          <Button
-            variant="outline"
-            @click.prevent="useSupabaseStore().deleteTag(tag.id)"
-          >
-            <Icon
-              icon="solar:trash-bin-minimalistic-outline"
-              width="24"
-              height="24"
+      <TableBody>
+        <TableRow v-for="tag in useSupabaseStore().tags" :key="tag.id">
+          <TableCell class="font-medium">
+            <Input
+              type="text"
+              @change="useSupabaseStore().updateTag(tag, tag.id)"
+              v-model="tag.name"
             />
-          </Button>
-        </TableCell>
-      </TableRow>
-    </TableBody>
-  </Table>
+          </TableCell>
+          <TableCell
+            ><Input
+              @change="useSupabaseStore().updateTag(tag, tag.id)"
+              type="text"
+              v-model="tag.description"
+            />
+          </TableCell>
+          <TableCell class="text-right">
+            <Button
+              variant="outline"
+              @click.prevent="useSupabaseStore().deleteTag(tag.id)"
+            >
+              <Icon
+                icon="solar:trash-bin-minimalistic-outline"
+                width="24"
+                height="24"
+              />
+            </Button>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </Card>
 </template>
